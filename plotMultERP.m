dfb7dbcb9b259464e8eaed207b8a8e853c17aec0
Laurens R Krol <lrkrol@gmail.com>
2017-04-05 13:48:27 -0700

    Version 2015-11-06

--- plotMultERP.m
+++ plotMultERP.m
@@ -1,15 +1,16 @@
-% h = plotDiffERP(epochs1, epochs2, channel [, name1, name2, delaycorrection, yticksize, vscale, xscalepos, legendpos, smoothing])
+% h = plotMultERP(epochs, channel [, names, colors, delaycorrection, yticksize, vscale, xscalepos, legendpos, smoothing])
 %
 % In:
-%       epochs1 - 1-by-n cell of epoched EEG set(s) to average and plot.
-%                 multiple sets are first averaged within, then across sets.
-%       epochs2 - 1-by-m cell of epoched EEG set(s) to average, plot, and
-%                 subtract from epochs1; the difference will also be plotted.
+%       epochs - 1-by-n cell containing 1-by-m cells of epoched EEG set(s)
+%                to average and plot. multiple sets are first averaged within,
+%                then across sets. i.e., the length of epochs determines the
+%                amount of ERPs plotted, the length of epochs{1} the amount of
+%                sets averaged together for the first ERP.
 %       channel - index of channel to plot
 %
 % Optional:
-%       name1 - legend entry for the first curve (default 'condition 1')
-%       name2 - legend entry for the second curve (default 'condition 2')
+%       names - cell of legend entries for the ERPs (default: no legend)
+%       colors - array of colors for the ERPs (default hsv(length(epochs)))
 %       delaycorrection - delay to visually correct for in seconds, i.e.
 %                         where the zero point should be on the x axis
 %       yticksize - scale of the y-axis, in microvolts (default 0 attempts
@@ -35,67 +36,54 @@
 %       h - handle of the generated figure
 %
 % Usage example:
-%       >> plotDiffERP({ALLEEG(1), ALLEEG(3)}, {ALLEEG(2), ALLEEG(4)}, 14)
+%       >> plotMultERP({{EEGa1, EEGa2}, {EEGb1}, {EEGc1}}, 14, {'case a',
+%          'case b', 'case c'}, winter(3), 0.25)
 % 
 %                       Laurens R Krol, 2015
 %                       Team PhyPA, Department of Biological Psychology and
 %                       Neuroergonomics, Berlin Institute of Technology
 
-% 2015-11-06 lrk
-%   - Instead of single EEG sets, epoch1 and epoch2 are now cells that may
-%     contain more than one set each (e.g. one for each subject)
-%   - Fixed a bug where sometimes, text() coordinates were not doubles
-% 2015-06-24 lrk
-%   - Fixed bug where the channel index for the chanlabel was hardcoded to 5
-%   - Fixed vertical alignment of labels that vscale caused to lose alignment
-%   - Forced signs on y scale labels
-% 2015-05-29 First version
+% 2015-11-06 First version
 
-function h = plotDiffERP(epochs1, epochs2, channel, name1, name2, delaycorrection, yticksize, vscale, xscalepos, legendpos, smoothing)
+function h = plotMultERP(epochs, channel, names, colors, delaycorrection, yticksize, vscale, xscalepos, legendpos, smoothing)
 
 % setting defaults
+if (~exist('colors', 'var')); colors = hsv(length(epochs)); end
 if (~exist('delaycorrection', 'var')); delaycorrection = 0; end
-if (~exist('name1', 'var')); name1 = 'condition 1'; end
-if (~exist('name2', 'var')); name2 = 'condition 2'; end
 if (~exist('yticksize', 'var')); yticksize = 0; end
 if (~exist('vscale', 'var')); vscale = 1; end
 if (~exist('xscalepos', 'var')); xscalepos = 7; end
 if (~exist('legendpos', 'var')); legendpos = 4; end
+if (~exist('names', 'var')); legendpos = 0; end
 if (~exist('smoothing', 'var')); smoothing = 'on'; end
 
-% averaging over epochs
-erp1 = [];
-for n = 1:length(epochs1)
-    erp1 = [erp1; mean(epochs1{n}.data(channel,:,:), 3)];
+% getting mean ERPs
+erps = [];
+for n = 1:length(epochs)
+    conditionerp = [];
+    for m = 1:length(epochs{n})
+        conditionerp = [conditionerp; mean(epochs{n}{m}.data(channel,:,:), 3)];
+    end
+    erps = [erps; mean(conditionerp, 1)];
 end
-erp1 = mean(erp1, 1);
-
-erp2 = [];
-for n = 1:length(epochs2)
-    erp2 = [erp2; mean(epochs2{n}.data(channel,:,:), 3)];
-end
-erp2 = mean(erp2, 1);
-
-diff = erp1 - erp2;
 
 % applying delay correction
-xmin = epochs1{1}.xmin - delaycorrection;
-xmax = epochs1{1}.xmax - delaycorrection;
+xmin = epochs{1}{1}.xmin - delaycorrection;
+xmax = epochs{1}{1}.xmax - delaycorrection;
 
 % getting x values
-x = xmin:1/epochs1{1}.srate:xmax;
+x = xmin:1/epochs{1}{1}.srate:xmax;
 
 % drawing figure
 h = figure('Color', [1 1 1]);
-colors = [4/255 129/255 255/255; 53/255 181/255 0/255; 255/255 4/255 141/255];
-curve1 = plot(x, erp1, 'Color', colors(1,:), 'LineSmoothing', smoothing);
 hold on;
-curve2 = plot(x, erp2, 'Color', colors(2,:), 'LineSmoothing', smoothing);
-curved = plot(x, diff, 'Color', colors(3,:), 'LineSmoothing', smoothing);
+for i = 1:size(erps, 1)
+    curves(i) = plot(x, erps(i,:), 'Color', colors(i,:), 'LineSmoothing', smoothing);
+end
 
 % getting data ranges
-ymax = max([erp1, erp2, diff]);
-ymin = min([erp1, erp2, diff]);
+ymax = max(max(erps));
+ymin = min(min(erps));
 yrange = ymax - ymin;
 xrange = xmax - xmin;
 
@@ -179,7 +167,7 @@ xaxislabel = line(xaxislabelx, xaxislabely, 'Color', [0 0 0]);
 labelx = text(double(textposx), double(textposy), [num2str(xsize) 's'], 'VerticalAlignment', 'baseline', 'HorizontalAlignment', 'center');
 
 % drawing channel name
-chanlabel = text(0, double(yticksize * 1.4 * vscale^1.35), ['\bf' epochs1{1}.chanlocs(channel).labels], 'HorizontalAlignment', 'center');
+chanlabel = text(0, double(yticksize * 1.4 * vscale^1.35), ['\bf' epochs{1}{1}.chanlocs(channel).labels], 'HorizontalAlignment', 'center');
 
 % drawing legend
 if legendpos == 0
@@ -202,12 +190,19 @@ else
             x = xmax;
             align = 'right';
     end
-
-    legend = text(double(x), double(yticksize * -1.35 * vscale^1.35), ['\color[rgb]{' num2str(colors(1,:)) '}' name1 char(10) '\color[rgb]{' num2str(colors(2,:)) '}' name2 char(10) '\color[rgb]{' num2str(colors(3,:)) '}difference'] , 'Color', colors(1,:), 'HorizontalAlignment', align, 'VerticalAlignment', 'top');
+    
+    legendtext = [];
+    for i = 1:size(erps, 1)
+        legendtext = [legendtext, '\color[rgb]{' num2str(colors(i,:)) '}' names{i}];
+        if i < size(erps, 1)
+            legendtext = [legendtext char(10)];
+        end
+    end
+    legend = text(double(x), double(yticksize * -1.35 * vscale^1.35), legendtext, 'HorizontalAlignment', align, 'VerticalAlignment', 'top');
 end
 
 % setting figure drawing order, removing original axes
-set(gca, 'Children', [curve1, curve2, curved, xaxis, yaxis, labelyp, labelyn, xaxislabel, labelx, chanlabel, legend]);
+set(gca, 'Children', [curves, xaxis, yaxis, labelyp, labelyn, xaxislabel, labelx, chanlabel, legend]);
 set(gca, 'Visible', 'off');
 
 end
